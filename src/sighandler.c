29be08066fd049ad1ec591ebba114eecf6dc1c05
harooonm <butterjacksays@gmail.com>
2019-03-28 14:13:30 +0000

    removed extraneous copy pointer

--- src/sighandler.c
+++ src/sighandler.c
@@ -24,21 +24,21 @@ struct tree_node {
 	struct tree_node *right;
 }attrib(packed);
 
-static struct tree_node *root;
+static struct tree_node root;
 static pthread_mutex_t mtx;
 static struct sigaction action;
 
-static struct tree_node nonnull(1) **find_sig_node(struct tree_node **n,
+static struct tree_node nonnull(1) *find_sig_node(struct tree_node *n,
                 int sig_nr)
 {
-	struct tree_node **node_ptr = n;
-	while (*node_ptr) {
-		if ((*node_ptr)->sig_nr == sig_nr)
+	struct tree_node *node_ptr = n;
+	while (node_ptr) {
+		if (node_ptr->sig_nr == sig_nr)
 			return node_ptr;
-		if (sig_nr > (*node_ptr)->sig_nr)
-			node_ptr = &(*node_ptr)->right;
+		if (sig_nr > node_ptr->sig_nr)
+			node_ptr = node_ptr->right;
 		else
-			node_ptr = &(*node_ptr)->left;
+			node_ptr = node_ptr->left;
 	}
 	return NULL;
 }
@@ -46,9 +46,9 @@ static struct tree_node nonnull(1) **find_sig_node(struct tree_node **n,
 static void _os_sig_handler(int sig_nr, siginfo_t *info, void *e)
 {
 	pthread_mutex_lock(&mtx);
-	struct tree_node **n = find_sig_node(&root, sig_nr);
+	struct tree_node *n = find_sig_node(&root, sig_nr);
 	if (n) {
-		struct list_node *ln = (*n)->handlers;
+		struct list_node *ln = n->handlers;
 		while (ln) {
 			if (info && e && ln->sigact_handler)
 				ln->sigact_handler(sig_nr, info, e);
@@ -72,54 +72,58 @@ static void os_sig_acthandler(int sig_nr, siginfo_t *info, void *e)
 
 static void attrib(constructor) construct()
 {
+	root.sig_nr = -1;
+	root.left = NULL;
+	root.right = NULL;
+	root.old_act = NULL;
+	root.handlers = NULL;
 	pthread_mutex_init(&mtx, NULL);
 }
 
-static void free_tree(struct tree_node **n)
+static void free_tree(struct tree_node *n)
 {
-	if (!*n)
+	if (!n)
 		return;
-	free_tree(&(*n)->left);
-	free_tree(&(*n)->right);
+	free_tree(n->left);
+	free_tree(n->right);
 
 	/*free the list*/
-	//struct list_node *ln = (*n)->handlers;
-	while((*n)->handlers) {
-		struct list_node *nn = (*n)->handlers->next;
-		free((*n)->handlers);
-		(*n)->handlers = nn;
+	while(n->handlers) {
+		struct list_node *nn = n->handlers->next;
+		free(n->handlers);
+		n->handlers = nn;
 	}
 	/*restore old action*/
-	sigaction((*n)->sig_nr, (*n)->old_act, NULL);
+	sigaction(n->sig_nr, n->old_act, NULL);
 
 	/*free the tree node*/
-	free(*n);
+	free(n);
 }
 
-static void nonnull(1) del_node(struct tree_node **node)
+static void nonnull(1) del_node(struct tree_node *node)
 {
 
 	if (!node)
 		return;
-	if ((*node)->left && (*node)->right) {
-		struct tree_node **follow = &(*node)->right;
+	if (node->left && node->right) {
+		struct tree_node *follow = node->right;
 		while (1) {
-			if (!(*follow)->left)
+			if (!follow->left)
 				break;
-			follow = &(*follow)->left;
+			follow = follow->left;
 		}
-		(*node)->sig_nr = (*follow)->sig_nr;
-		free(*follow);
-		*follow = NULL;
-	} else if ((*node)->left || (*node)->right) {
+		node->sig_nr = follow->sig_nr;
+		free(follow);
+		follow = NULL;
+	} else if (node->left || node->right) {
 		struct tree_node *cp =
-		                (*node)->left ? (*node)->left : (*node)->right;
-		free(*node);
-		(*node) = NULL;
-		*node = cp;
+		                node->left ? node->left : node->right;
+		free(node);
+		node = NULL;
+		node = cp;
 	} else {
-		free(*node);
-		*node = NULL;
+		free(node);
+		node = NULL;
 	}
 }
 
@@ -157,25 +161,25 @@ static int find_handler(struct list_node *n, on_sigact act, on_sig sahandler)
 	return 0;
 }
 
-static int nonnull(1) add_node(struct tree_node **n, int sig_nr,
+static int nonnull(1) add_node(struct tree_node *n, int sig_nr,
                 on_sig sig_handler, on_sigact sig_acthandler, int blck_mask,
                 int flags)
 {
-	struct tree_node **node_ptr = n;
+	struct tree_node *node_ptr = n;
 
-	while (*node_ptr) {
-		if (sig_nr > (*node_ptr)->sig_nr)
-			node_ptr = &(*node_ptr)->right;
+	while (node_ptr && node_ptr->sig_nr != -1) {
+		if (sig_nr > node_ptr->sig_nr)
+			node_ptr = node_ptr->right;
 		else
-			node_ptr = &(*node_ptr)->left;
+			node_ptr = node_ptr->left;
 	}
 
-	*node_ptr = malloc(sizeof(struct tree_node));
-	(*node_ptr)->sig_nr = sig_nr;
-	(*node_ptr)->left = NULL;
-	(*node_ptr)->right = NULL;
-	(*node_ptr)->old_act = NULL;
-	(*node_ptr)->handlers = NULL;
+	node_ptr = malloc(sizeof(struct tree_node));
+	node_ptr->sig_nr = sig_nr;
+	node_ptr->left = NULL;
+	node_ptr->right = NULL;
+	node_ptr->old_act = NULL;
+	node_ptr->handlers = NULL;
 	sigaddset(&action.sa_mask, blck_mask);
 	action.sa_flags |= flags;
 
@@ -185,35 +189,35 @@ static int nonnull(1) add_node(struct tree_node **n, int sig_nr,
 	if (sig_handler)
 		action.sa_handler = os_sig_handler;
 
-	if (sigaction(sig_nr, &action, (*node_ptr)->old_act))
+	if (sigaction(sig_nr, &action, node_ptr->old_act))
 		return 0;
 
-	add_handler(&(*node_ptr)->handlers, sig_handler, sig_acthandler);
+	add_handler(&(node_ptr->handlers), sig_handler, sig_acthandler);
 
 	return 1;
 }
 
 
 
-static void remove_list_node(struct list_node **head, on_sig sahadler,
+static void remove_list_node(struct list_node *head, on_sig sahadler,
                 on_sigact sig_acthandler)
 {
-	struct list_node **cur = head;
-	while (*cur) {
-		if ((*cur)->sig_handler && (*cur)->sig_handler == sahadler)
-			(*cur)->sig_handler = NULL;
+	struct list_node *cur = head;
+	while (cur) {
+		if (cur->sig_handler && cur->sig_handler == sahadler)
+			cur->sig_handler = NULL;
 
-		if ((*cur)->sigact_handler
-		                && (*cur)->sigact_handler == sig_acthandler)
-			(*cur)->sigact_handler = NULL;
+		if (cur->sigact_handler
+		                && cur->sigact_handler == sig_acthandler)
+			cur->sigact_handler = NULL;
 
-		if (!(*cur)->sig_handler && !(*cur)->sigact_handler) {
-			struct list_node *n = (*cur)->next;
-			free((*cur));
-			*cur = n;
+		if (!cur->sig_handler && cur->sigact_handler) {
+			struct list_node *n = cur->next;
+			free(cur);
+			cur = n;
 			break;
 		}
-		cur = &(*cur)->next;
+		cur = cur->next;
 	}
 }
 
@@ -226,12 +230,12 @@ static int _reg_sig(int sig_nr, on_sig sig_handler, on_sigact sigact_handler,
 		return 0;
 
 	pthread_mutex_lock(&mtx);
-	struct tree_node **sig_node = find_sig_node(&root, sig_nr);
+	struct tree_node *sig_node = find_sig_node(&root, sig_nr);
 	int reged = 0;
 	if (sig_node != NULL) {
-		if (!find_handler((*sig_node)->handlers, sigact_handler,
+		if (!find_handler(sig_node->handlers, sigact_handler,
 		                sig_handler))
-			add_handler(&(*sig_node)->handlers, sig_handler,
+			add_handler(&(sig_node)->handlers, sig_handler,
 			                sigact_handler);
 		reged = 1;
 	} else {
@@ -247,10 +251,10 @@ static void _unreg_sig(int sig_num, on_sig sig_handler,
                 on_sigact sig_acthandler)
 {
 	pthread_mutex_lock(&mtx);
-	struct tree_node **n = find_sig_node(&root, sig_num);
+	struct tree_node *n = find_sig_node(&root, sig_num);
 	if (n) {
-		remove_list_node(&(*n)->handlers, sig_handler, sig_acthandler);
-		if (!(*n)->handlers){
+		remove_list_node(n->handlers, sig_handler, sig_acthandler);
+		if (!n->handlers){
 			del_node(n);
 		}
 	}
